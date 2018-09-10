0cf304c5d65b8ec92ec743dcf361ac0a7cce0aa0
Brighton Muffat <34190068+brightonm@users.noreply.github.com>
2018-09-10 11:20:37 +0200

    Add files via upload

---
+++ project_2_using_sentiment_analysis_to_predict_world_cup_winner/project.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+'''
+Project : Create a labeled world cup tweets dataset in csv file for each team
+in order to predict the winner of the competition via sentiment analysis (using NLP)
+'''
+
+import tweepy
+import numpy as np
+import pandas as pd
+from textblob import TextBlob # NLP
+
+# Step 1 - Authenticate (Twitter API)
+
+consumer_key = 'ad29zgKmJ269rkQ5B2VlC1a3i'
+consumer_secret = 'T8of85b1nVIcCfzONOezHK1A9803yNZcRyfxCc7dnCgcIGXu8i'
+
+access_token = '1014588628079644672-U38vSJaMNMb88wUu53NllDe7ZvPGJp'
+access_token_secret = 'lyjvr7rAROGVEUF6MOnEyjzHAAQZ4LXtxw0pu7rq4th4x'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+
+# Step 2 - Create pandas dataframe with the values
+
+teams = ['Argentina', 'Australia', 'Belgium', 'Brazil', 'Colombia', 'Costa Rica', 'Croatia', 'Denmark',
+         'Egypt', 'England', 'France', 'Germany', 'Iceland', 'Iran', 'Japan', 'Korea Republic',
+         'Mexico', 'Morocco', 'Nigeria', 'Panama', 'Peru', 'Poland', 'Portugal', 'Russia',
+         'Saudi Arabia', 'Senegal', 'Serbia', 'Spain', 'Sweden', 'Switzerland', 'Tunisia', 'Uruguay']
+
+
+def get_label(analysis, threshold = 0):
+    '''
+    Labelisation of tweets
+    '''
+    if analysis.sentiment.polarity > threshold:
+        return 'Positive'
+    else:
+        return 'Negative'
+
+all_polarities = dict() # Predict the winner
+
+for team in teams:
+    team_polarities = []
+    # Create the DataFrame
+    df = pd.DataFrame(columns ={'Tweet', 'Sentiment Polarity', 'Subjectivity', 'Label'})
+
+    # Get the tweets about the teams
+    team_tweets = api.search(q=[team, 'World Cup'], lang='en', count='50')
+    # Append the features and labels
+    for tweet in team_tweets:
+        text = tweet.text
+        # print(text)
+        analysis = TextBlob(text)
+        polarity = analysis.sentiment.polarity
+        team_polarities.append(polarity)
+        subjectivity = analysis.sentiment.subjectivity
+        label = get_label(analysis)
+        df = df.append({'Tweet':text, 'Sentiment Polarity':polarity, 'Subjectivity':subjectivity, 'Label':label}, ignore_index=True) #append rows
+
+    # Update all_polarities
+    all_polarities[team] = np.mean(team_polarities)
+
+    # Convert pandas dataframe to csv
+    df.set_index('Tweet', inplace=True)
+    df.to_csv('{}_tweets_analysed.csv'.format(team))
+
+# printed the next winner, the one with the higher polarity
+
+world_cup_winner = max(all_polarities, key=all_polarities.get)
+winner_polarity = all_polarities[world_cup_winner]
+print('The 2018 World Cup is {} with {} of polarity'.format(world_cup_winner, winner_polarity))
